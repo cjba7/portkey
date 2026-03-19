afafccad1f8ff2f4d9793b35978d5dedf0325c70
Carlos B <4706739+cjba7@users.noreply.github.com>
2026-03-19 14:48:15 +0100

    update formula to v1.0.1

--- Formula/portkey.rb
+++ Formula/portkey.rb
@@ -1,8 +1,8 @@
 class Portkey < Formula
   desc "System-wide port registry for developers running multiple projects"
   homepage "https://github.com/cjba7/portkey"
-  url "https://github.com/cjba7/portkey/archive/refs/tags/v1.0.0.tar.gz"
-  sha256 "f58b42bf0a32dd2136c4e8aeebab95c4c5b58d5705947e656f95d58fdeeeba8b"
+  url "https://github.com/cjba7/portkey/archive/refs/tags/v1.0.1.tar.gz"
+  sha256 "9a6bdc65835422a53351381d16f74bbd39b3825614242e5479262a53a602cea1"
   license "MIT"
 
   depends_on "direnv" => :recommended
